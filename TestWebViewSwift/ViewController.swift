7c2bb8399f33a6ba9ea43772fb563ad3487dfe95
Ivan Kramarchuk <night.silf@gmail.com>
2017-05-03 22:53:53 +0300

    First

--- TestWebViewSwift/ViewController.swift
+++ TestWebViewSwift/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        webView.loadRequest(URLRequest(url: URL(string: "http://ya.ru")!))
+                if let url = NSURL(string: "http://google.com/"){
+                    let request = NSURLRequest(url: url as URL)
+                    // webView.scalesPageToFit = true
+                    webView.loadRequest(request as URLRequest)
+                }
     }
 
     override func didReceiveMemoryWarning() {
